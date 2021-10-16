6c0609f9a1c76f0c73d6a21fafba7ee2c98a9812
khmalz <akmalrma1710@gmail.com>
2021-10-16 21:32:54 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const buttonTambah = document.querySelector(".button-tambah");
+const inputTambah = document.querySelector(".inputby");
+var minChar = 3;
+
+inputTambah.addEventListener("keypress", function (e) {
+   // Menambahkan list nya
+   if (e.key === "Enter") {
+      if (inputTambah.value == null || inputTambah.value == "") {
+         alert("Isi dulu");
+         inputTambah.value = "";
+      } else if (inputTambah.value.length < minChar) {
+         alert("Minimal isi dengan 3 huruf");
+      } else {
+         let list = "";
+         list += listB();
+         const ul = document.querySelector(".list-group");
+         ul.insertAdjacentHTML("afterbegin", list);
+         //   Menghapus teks dalam input
+      }
+      inputTambah.value = "";
+   } else if (e.code === "Space") {
+      if (inputTambah.value == null || inputTambah.value == "") {
+         alert("Isi dulu");
+      } else if (inputTambah.value.length < minChar) {
+         alert("Minimal isi dengan 3 huruf");
+      } else {
+         let list = "";
+         list += listB();
+         const ul = document.querySelector(".list-group");
+         ul.insertAdjacentHTML("afterbegin", list);
+         //   Menghapus teks dalam input
+      }
+      inputTambah.value = null;
+   }
+});
+
+buttonTambah.addEventListener("click", function () {
+   if (inputTambah.value == null || inputTambah.value == "") {
+      alert("Isi dulu");
+   } else if (inputTambah.value.length < minChar) {
+      alert("Minimal isi dengan 3 huruf");
+   } else {
+      let list = "";
+      list += listB();
+      const ul = document.querySelector(".list-group");
+      ul.insertAdjacentHTML("afterbegin", list);
+      //   Menghapus teks dalam input
+   }
+   inputTambah.value = "";
+});
+
+function remove(el) {
+   el.parentElement.remove();
+}
+
+function listB() {
+   return `<li class="list-group-item d-flex justify-content-between align-items-start">
+   <div class="ms-2 my-auto me-auto judul">
+      <div class="fw-bold">${inputTambah.value}</div>
+   </div>
+   <button class="btn btn-danger hapus" onclick="remove(this)" type="button" id="button-addon2"><i class="bi bi-trash-fill"></i></button>
+</li>`;
+}
